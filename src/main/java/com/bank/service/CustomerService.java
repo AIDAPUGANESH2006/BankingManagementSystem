b87d1bed91717003279742d3243bf1755e6fc994
AIDAPUGANESH2006 <ganeshaidapu@gmail.com>
2026-07-03 19:45:10 +0530

    Added Customer Service methods

--- src/main/java/com/bank/service/CustomerService.java
+++ src/main/java/com/bank/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.bank.service;
 
 import com.bank.dao.CustomerDAO;
 import com.bank.model.Customer;
+import java.util.List;
 
 public class CustomerService {
 
@@ -10,4 +11,9 @@ public class CustomerService {
     public boolean addCustomer(Customer customer) {
         return customerDAO.addCustomer(customer);
     }
+
+    public List<Customer> getAllCustomers() {
+        return customerDAO.getAllCustomers();
+    }
 }
+
